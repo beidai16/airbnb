ef1c04420aaf8bf46256426b08e0b32c0b6b959d
Bei Dai <bei.dai@kellogg.northwestern.edu>
2023-04-14 19:45:16 +0000

    6

--- practice-6.sql
+++ practice-6.sql
@@ -15,6 +15,8 @@
 -- | Brighton Park          | 678      |
 -- | Burnside               | 10       |
 
-SELECT neighborhood, COUNT(*) FROM reviews
+SELECT listings.neighborhood, COUNT(reviews.id) FROM reviews
+LEFT JOIN listings ON listings.id = reviews.listing_id
 GROUP BY property_type
+ORDER BY listings.neighborhood
 ;
